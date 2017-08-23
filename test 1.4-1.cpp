280d9214bb67b8183eea5d36e1e1a27720671c65
邪美公子 <xiemeigongzi@users.noreply.github.com>
2017-08-23 13:42:53 -0500

    Add files via upload

---
+++ test 1.4-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Enter two nunbers:"<<endl;
+	int v1=0, v2=0;
+	cin>>v1>>v2;
+	cout<<"The product of two numbers is "<<v1*v2<<endl;
+	
+	return 0;
+}
